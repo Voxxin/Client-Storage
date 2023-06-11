6134886352e1a23d2e5563c90144952d37205fc3
Ella <59192970+Voxxin@users.noreply.github.com>
2023-06-11 18:14:26 -0400

    whack

--- src/main/java/com/github/voxxin/clientstorage/client/ModConfig.java
+++ src/main/java/com/github/voxxin/clientstorage/client/ModConfig.java
@@ -6,6 +6,7 @@ import net.minecraft.block.Block;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 
